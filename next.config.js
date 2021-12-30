c91231fc2ee4b5601245f3b42e4bf861ce8b56d1
Jeongyong Park <kladess@gmail.com>
2021-12-30 10:28:05 +0900

    prod check

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+const { env } = require('process')
+
+const prod = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  assetPrefix: prod?'Korea-UV-Repeater-Map':'',
   webpack: (config, options) => {
     config.module.rules.push({
         test: /\.csv$/,
