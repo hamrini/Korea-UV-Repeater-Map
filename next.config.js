1326e98b7bbfb83612c32b97b302459a34547a6a
Jeongyong Park <kladess@gmail.com>
2021-12-31 13:27:40 +0900

    fixed for github action

--- next.config.js
+++ next.config.js
@@ -1,22 +1,9 @@
 const { env } = require('process')
 
-const prod = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: prod?'/Korea-UV-Repeater-Map/':'',
-  webpack: (config, options) => {
-    config.module.rules.push({
-        test: /\.csv$/,
-        loader: 'csv-loader',
-        options: {
-          dynamicTyping: true,
-          header: true,
-          skipEmptyLines: true
-        }
-      })
-
-    return config
-  }
+  assetPrefix: isProd ? '/Korea-UV-Repeater-Map/' : '',
 }
