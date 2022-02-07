02c87ce91f122f73715aac722fdd2b19e5a9c4ec
Jeongyong Park <kladess@gmail.com>
2022-02-07 23:37:04 +0900

    ㅇㅇ

--- next.config.js
+++ next.config.js
@@ -5,6 +5,6 @@ const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
+  
   assetPrefix: isProd ? '/Korea-UV-Repeater-Map/' : '',
 }
